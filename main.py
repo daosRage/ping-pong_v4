3b5572f4fde08d3ed8501618ae5c12890b60ff0e
daosRage <r1a2g3e4@gmail.com>
2023-09-01 18:16:20 +0300

    2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from pong import *
+from PyQt5.QtWidgets import QInputDialog, QApplication
 
+app = QApplication([])
 pygame.init()
 
 setting_win = (700, 500)
@@ -21,17 +23,23 @@ player_right = Board(setting_win[0] - setting_board[0] - 10,
                      setting_board[1], 
                      (255,255,255), 
                      5)
+player_left.NAME = QInputDialog().getText(QInputDialog(), "PING-PONG", "Введіть ім'я лівого гравця")[0][:10].lower()
+player_right.NAME = QInputDialog().getText(QInputDialog(), "PING-PONG", "Введіть ім'я правого гравця")[0][:10].lower()
+ball = Ball(setting_win[0] // 2 - 20, setting_win[1] // 2 - 20, 20, (255,255,200), 5)
 
 clock = pygame.time.Clock()
 
 game = True
 while game:
     window.fill((0,0,0))
-
+    pygame.draw.line(window, (255, 255, 255), (setting_win[0] // 2, 0), (setting_win[0]// 2, setting_win[1]), 3)
+    blit_player_score(window, player_left, player_right, setting_win)
     player_left.move(window)
     player_right.move(window)
+    ball.move(window, player_left, player_right)
 
-    pygame.draw.line(window, (255, 255, 255), (setting_win[0] // 2, 0), (setting_win[0]// 2, setting_win[1]), 3)
+
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- pong.py
+++ pong.py
@@ -1,13 +1,15 @@
 import pygame
+from random import uniform
 
 class Board(pygame.Rect):
-    def __init__(self, x, y, width, height, color, speed, name= "player"):
+    def __init__(self, x, y, width, height, color, speed, name= "playerj,hsbg,d"):
         super().__init__(x, y, width, height)
         self.COLOR = color
         self.SPEED = speed
         self.NAME = name
         self.SCORE = 0
         self.MOVE = {"UP": False, "DOWN": False}
+        self.FONT = pygame.font.SysFont("Comic Sans MS", 40)
 
     def move(self, window):
         if self.MOVE["UP"] and self.y > 0:
@@ -15,3 +17,61 @@ class Board(pygame.Rect):
         elif self.MOVE["DOWN"] and self.y + self.height < 500:
             self.y += self.SPEED
         pygame.draw.rect(window, self.COLOR, self)
+
+def blit_player_score(window, player_left, player_right, setting_win):
+    render_score_left = player_left.FONT.render(str(player_left.SCORE), True, (0, 63, 255))
+    render_score_right = player_right.FONT.render(str(player_right.SCORE), True, (0, 63, 255))
+    render_name_left = player_left.FONT.render(player_left.NAME, True, (0, 63, 255))
+    render_name_right = player_right.FONT.render(player_right.NAME, True, (0, 63, 255))
+
+    window.blit(render_score_left, (setting_win[0] // 2 - 50, 5))
+    window.blit(render_score_right, (setting_win[0] // 2 + 20, 5))
+    window.blit(render_name_left, (5, 5))
+    window.blit(render_name_right, (setting_win[0] - player_right.FONT.size(player_right.NAME)[0] - 5, 5))
+
+class Ball(pygame.Rect):
+    def __init__(self, x, y, radius, color, speed):
+        super().__init__(x, y, radius * 2, radius * 2)
+        self.RADIUS = radius
+        self.COLOR = color
+        self.SPEED = speed
+        self.SPEED_X = self.SPEED
+        self.SPEED_Y = 0
+        self.COUNT = 0
+    
+    def move(self, window, player_left, player_right):
+        pygame.draw.circle(window, self.COLOR, (self.x + self.RADIUS, self.y + self.RADIUS), self.RADIUS)
+
+        if self.y < 0 or self.y + self.height > 500:
+            self.SPEED_Y *= -1
+        if self.colliderect(player_left) and self.COUNT > 5:
+            self.COUNT = 0
+            if player_left.MOVE["UP"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2 + self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+                self.SPEED_Y *= -1
+            elif player_left.MOVE["DOWN"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2 + self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+                
+            self.SPEED_X *= -1
+        elif self.colliderect(player_right):
+            self.COUNT = 0
+            if player_right.MOVE["UP"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2 + self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+                self.SPEED_Y *= -1
+            elif player_right.MOVE["DOWN"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2 + self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+
+            self.SPEED_X *= -1
+
+        self.x += self.SPEED_X
+        self.y += self.SPEED_Y
+        self.COUNT += 1
+        
